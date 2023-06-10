17c56f32761c97ccc05e8512b1b43f02631398fa
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-06-10 11:03:00 +0530

    Update detector.py

--- detector.py
+++ detector.py
@@ -34,3 +34,19 @@ def recognize_faces(
         if not name:
             name = "Unknown"
         print(name, bounding_box)
+
+
+def _recognize_face(unknown_encoding, loaded_encodings):
+    boolean_matches = face_recognition.compare_faces(
+        loaded_encodings["encodings"], unknown_encoding
+    )
+    votes = Counter(
+        name
+        for match, name in zip(boolean_matches, loaded_encodings["names"])
+        if match
+    )
+    if votes:
+        return votes.most_common(1)[0][0]
+
+
+recognize_faces("unknown.jpg")
